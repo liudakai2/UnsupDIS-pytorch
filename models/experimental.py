99f36c9fbaab81cbfdb6cebbc1111654979dc892
tgjjj <35397867+tgjjj@users.noreply.github.com>
2022-06-23 17:29:48 +0800

    Fixed bug that in test.py the model has no attribute 'hyp'

--- models/experimental.py
+++ models/experimental.py
@@ -118,6 +118,7 @@ def attempt_load(weights, map_location=None, inplace=True):
     for w in weights if isinstance(weights, list) else [weights]:
         ckpt = torch.load(attempt_download(w), map_location=map_location)  # load
         model.append(ckpt['ema' if ckpt.get('ema') else 'model'].float().fuse().eval())  # FP32 model
+        setattr(model[-1], 'hyp', ckpt['model'].hyp)
 
     # Compatibility updates
     for m in model.modules():
